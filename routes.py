5f8daa46aa2a3bc5372d2929bd0cca55007a5a54
Sean Ankenbruck <Sean Ankenbruck>
2017-02-22 22:48:15 -0500

    database connected

--- routes.py
+++ routes.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
-from models import db
+from models import db, User
 from forms import SignupForm
 
 app = Flask(__name__)
 
 # Connect the postgresql database to our application
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:R0meo0117@localhost:5432/learningflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
@@ -22,8 +23,15 @@ def about():
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
 	form = SignupForm()
+
 	if request.method == 'POST':
-		return "Success!"
+		if form.validate() == False:
+			return render_template('signup.html', form=form)
+		else:
+			newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+			db.session.add(newuser)
+			db.session.commit()
+			return "Success!"
 	elif request.method == 'GET':	
 		return render_template('signup.html', form=form)
 
